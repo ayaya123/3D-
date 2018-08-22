a5979d73ee5cabee56df37859473f43d6f0c784b
lensherrggg <gr98522@gmail.com>
2018-08-22 15:00:44 +0800

    Add first version and gitignore

---
+++ BomberMan/Assets/Scripts/DestructibleBlocks.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleBlocks : MonoBehaviour {
+
+    public GameObject bombPrefab;
+
+    private Rigidbody rigidBody;
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Explosion"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
