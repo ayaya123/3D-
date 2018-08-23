5c2c3b6fd6dd23aef58fdf549bb31a1364c28338
ayaya123 <1149897723@qq.com>
2018-08-23 16:19:55 +0800

    xx

---
+++ 3D炸弹超人游戏/BomberMan/Assets/Scripts/GlobalStateManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GlobalStateManager : MonoBehaviour
+{
+    private int deadPlayers = 0;
+    private int deadPlayerNumber = -1;
+    public GameObject GO;
+    public GUIText guit;
+
+
+    public void PlayerDied (int playerNumber)
+    {
+        deadPlayers++; 
+
+        if (deadPlayers == 1)
+        { 
+            deadPlayerNumber = playerNumber;
+            CheckPlayersDeath();
+        }
+    }
+
+    void CheckPlayersDeath()
+    {
+        if (deadPlayers == 1)
+        {
+            if (deadPlayerNumber == 1)
+            {
+                Application.LoadLevel("Stage2");
+            }
+            if (deadPlayerNumber == 2)
+            {
+                Application.LoadLevel("Stage3");
+            }
+        }
+    }
+
+}
+
---
+++ 3D炸弹超人游戏/README.md
@@ -0,0 +1,5 @@
+# 3D Bomberman
+## Update Log
+8/22 14:57 First version. Player can snap bombs in appropriate places and bombs can create explosions in different directions. 
+
+8/22 19:35 Player could be killed by explosion. Destructible blocks added.
